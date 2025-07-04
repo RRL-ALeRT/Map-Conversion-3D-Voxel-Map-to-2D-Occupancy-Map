bcf0503b5a36eb18feefcc755fe39d90808dfe14
Ninad Kulkarni <ninad.kulkarni@alumni.fh-aachen.de>
2025-07-04 15:57:13 +0200

    configure for Spot

--- mapconversion/launch/map_conversion.launch.py
+++ mapconversion/launch/map_conversion.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import math
 
 def generate_launch_description():
     return LaunchDescription([
@@ -10,19 +11,30 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'map_frame': 'map',
-                'minimum_z': 1.0,
-                'max_slope_ugv': 0.2,
-                'slope_estimation_size': 2,
-                'minimum_occupancy': 10,
+                'minimum_z': 0.35,
+                'max_slope_ugv': math.radians(10.0),
+                'slope_estimation_size': 1,  
+                'minimum_occupancy': 100,
+                'map_position_z': 0.0,
                 'partial_map_updates': True,
-                #QoS parameters
+                # QoS parameters
                 'subscriber_qos_reliable': True,
                 'subscriber_qos_transient_local': False,
                 'publisher_qos_reliable': True,
                 'publisher_qos_transient_local': False,
             }],
             remappings=[
-                ('octomap', 'octomap_binary')
+                ('octomap', 'octomap_full')
             ]
-        )
+        ),
+        # Uncomment if using offline map
+        # Node(
+        #     package='octomap_server',
+        #     executable='octomap_server_node',
+        #     name='octomap_server',
+        #     output='screen',
+        #     parameters=[{
+        #         'octomap_path': '/path/to/octomap.bt'
+        #     }]
+        # )
     ])
--- mapconversion/launch/path_conversion.launch.py
+++ mapconversion/launch/path_conversion.launch.py
@@ -15,8 +15,8 @@ def generate_launch_description():
             parameters=[{
                 'use_collision_sphere': False,
                 'collision_radius': 1.0,
-                'path_offset': 0.0},
-                'path_smothing_length': 5
+                'path_offset': 0.0,
+                'path_smothing_length': 5,
                 #QoS parameters
                 'subscriber_qos_reliable': True,
                 'subscriber_qos_transient_local': False,
